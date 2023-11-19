b3930bf5aab4365fd5f9969c077839f6a2d2b67a
eslam-obeidat <146520433+eslam-obeidat@users.noreply.github.com>
2023-11-19 08:22:40 -0800

    js part 2
    syntax

--- js/basics.js
+++ js/basics.js
@@ -91,3 +91,186 @@ age: 20,
 address: 'dhaka'
 }
 console.log(test3);
+
+
+// intro to js
+
+// syntax
+// data types
+// variables
+// conditionals
+// loops
+// functions
+
+// 1. syntax
+
+// 2. data types
+
+// string> contain text,num, symbols, spaces, etc but it must be in quotes
+// number> contain numbers
+// number > int float, double
+// boolean> true, false
+// array> collection of data > can contain any data type [data, data, data] index > 0,1,2,3,4,5,6,7,8,9
+// object> collection of data > can contain any data type (keytvalue, key:value, key: value)
+// null> empty> the variable is empty
+// undefined> the variable is not defined
+// NaN> not a number
+
+// 3. variables
+
+// var > global scope
+// var name = Value;
+
+// let > local scope
+// let name = Value;
+
+// const local scope
+// const name = Value;
+
+        var name = "ahmed";
+        var age1 = '40';
+                    // propertise : parseInt : To convert the variable to int
+        var newAge = parseInt(age1);
+        console.log(typeof(newAge));
+        console.log(age1);
+        let age = 20;
+        const isMarried = false;
+        const salary = 1000.5;
+        const skills = ["html", "css", "js"];
+        
+        var person = {
+        
+             name: "ahmad",
+             age: 25,
+             isMarried: true,
+             salary: 1500.5,
+             skills: ["html", "css", "js"]
+             }
+
+        console.log(typeof(name),'name:',name);
+        console.log(typeof(age),'age:',age);
+        console.log(typeof(isMarried),'isMarried:',isMarried);
+        console.log(typeof(salary),'salary:',salary);
+        console.log(typeof(skills),'skills:',skills);
+
+
+// 4. conditionals
+
+// 2 condetions if true > do something
+// else do something else
+
+// if (condition) {
+//        // do something
+
+// } else {
+//       //do something else
+
+var x = 10;
+var y = 20;
+
+if (x > y ){
+    console.log ('x is greater than y');
+}
+
+else {
+    console.log ('x is less than y');
+}
+
+var x = 30;
+var y = 20;
+
+if (x > y ){
+    console.log ('x is greater than y');
+    // alert ('x is greater than y');
+}
+
+else {
+    console.log ('x is less than y');
+    // alert ('x is less than y');
+}
+
+// 5. loops
+
+// for loop
+// while loop
+// do while loop
+
+// loop > repeat something code for many times
+// for loop > repeat something code for many times > for (start; end; step)
+// start > the start point for the loop
+// end > the end point for the loop
+// step > the step for the loop
+// for (start; end; step) {  
+    //        //de something
+// }
+
+// operations > + , - , * , / , % , ++ , --
+
+// x ++ > x = x + 1
+// x -- > x = x - 1
+// x += 5 > x = x + 5
+// x -= 5 > x = x - 5
+// ++x > x = x + 1
+// = , == , === , > , < , >= , <= , != , !== 
+
+        // = :  Assign or attribute a new value   
+        // == : comparison ( compare the value, not data type) ( check the value only)
+        // === : comparison (check the value and the data type)
+        // != : not equle ( check the value only)
+        // !== : not equle  (check the value and the data type)
+
+
+var x = 20
+
+        // console.log(x++);
+        console.log(++x);
+        console.log(x);
+        console.log(++x);
+
+// for loop
+    for (var i=0; i<10; i++){ 
+        console.log(i);
+    } 
+
+    for (var i=0; i<10; i++){ 
+        if ( i == 8) {
+            continue;       // continue : do skip or delete
+        }
+        console.log(i);
+    } 
+    for (var i=0; i<10; i++){ 
+        if ( i == 5) {
+            break;       // break : the function is to break the loop 
+        }
+        console.log(i);
+    } 
+
+
+var newSrt = "10";   //string
+var newNum = 10 ;   //number
+        console.log(newSrt == newNum );
+
+var newSrt = "10";   //string
+var newNum = 10 ;   //number
+        console.log(newSrt === newNum );        
+
+
+// while loop > repeat something code for many times while (condition)
+
+// while (condition) {
+    //        //dksomething
+// }
+
+var x = 60 
+
+while (x < 100){
+    console.log(x);
+    x++;
+}
+
+do {
+console.log(x);
+x++;
+}
+while (x < 100);
+ 
---
+++ js/skills.js
@@ -0,0 +1,12 @@
+
+            // propertise : innerHTML : Fetch the text inside the html tag
+            // propertise : textContant : Bring the text inside the content
+
+var footer = document.getElementById('footerP').innerHTML ;
+console.log(footer);
+
+
+var footer = document.getElementById('footerP');
+console.log(footer);
+footer.innerHTML = "Hello fram js";
+footer.style.color ="red";
